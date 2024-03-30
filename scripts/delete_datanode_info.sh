906256f572ec7ed02386dc147faadd33b6d7bd77
ych <canghaiyang@cqu.edu.cn>
2024-03-30 19:33:17 +0800

    tpch ok!

--- scripts/delete_datanode_info.sh
+++ scripts/delete_datanode_info.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 检查参数是否正确
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <start_node> <end_node> <dfs_directory>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <start_node> <end_node>"
     exit 1
 fi
 
 # 获取参数
 start_node="$1"
 end_node="$2"
-dfs_directory="$3"
+dfs_directory="/home/ych/hadoopData/dfs"
 
 # 循环遍历主机列表，执行命令
 for ((i=start_node; i<=end_node; i++)); do
